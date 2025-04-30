c5676fa217f737da18e208478422ddd8ded1ffbe
sHunOrinn <161719997+sHunOrinn@users.noreply.github.com>
2025-05-01 00:18:56 +0700

    customer point

--- src/Controller/StaffController/StaffJPanelController.java
+++ src/Controller/StaffController/StaffJPanelController.java
@@ -11,6 +11,8 @@ import java.util.Map;
 import javax.swing.JOptionPane;
 
 import Model.Product;
+import Repository.Customer.CustomerRepository;
+import Repository.Customer.ICustomerRespository;
 import Repository.Product.IProductRespository;
 import Repository.Product.ProductRespository;
 import Repository.Table.ITableRespository;
@@ -31,6 +33,7 @@ public class StaffJPanelController implements ActionListener {
         try {
             IProductRespository productRespository = new ProductRespository();
             ITableRespository tableRespository = new TableRepository();
+            ICustomerRespository customerRespository = new CustomerRepository();
             if (str.equals("Đặt món")) {
                 String formattedTime = time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
                 // JOptionPane.showMessageDialog(staffJPanel, "Chức năng này chưa được triển
@@ -38,7 +41,7 @@ public class StaffJPanelController implements ActionListener {
                 if (tableRespository.getTableStatus(staffJPanel.tableID).equals("Trống")) {
                     productRespository.addToOrder(staffJPanel.getTempOrderId(), staffJPanel.tableID,
                             staffJPanel.getEmpID(),
-                            100000, formattedTime);
+                            customerRespository.getCustomerIDByPhone(staffJPanel.customerPhone), formattedTime);
                     // Lưu từng món vào OrderDetail
                     for (Map.Entry<Product, Integer> entry : staffJPanel.getTempOrderProducts().entrySet()) {
                         Product product = entry.getKey();
