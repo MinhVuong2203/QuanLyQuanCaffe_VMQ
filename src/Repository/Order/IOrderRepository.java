c9d0b9f800d7bd32d1d78e8753fb8ec23cd668d3
quysutc2 <6451071063@st.utc2.edu.vn>
2025-04-30 10:51:45 +0700

    .

--- src/Repository/Order/IOrderRepository.java
+++ src/Repository/Order/IOrderRepository.java
@@ -3,12 +3,7 @@ package Repository.Order;
 import Model.Order;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 
 public interface IOrderRepository {
-    void addOrder(Order order) throws SQLException;
-    void deleteOrder(int orderId, int tableId) throws SQLException;
-    int getOrderIdByTableId(int tableId) throws SQLException;
-    void updateOrder(Order order) throws SQLException;
     List<Order> getAllOrders() throws SQLException;
 }
--- src/Repository/Order/OrderRepository.java
+++ src/Repository/Order/OrderRepository.java
@@ -1,5 +1,6 @@
 package Repository.Order;
 import Model.Order;
+import Repository.Product.ProductRespository;
 import Utils.JdbcUtils;
 import java.io.IOException;
 import java.sql.Connection;
@@ -7,99 +8,60 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class OrderRepository implements IOrderRepository {
+
+   
     private JdbcUtils jdbcUtils;
     private Connection connection;
     public OrderRepository () throws IOException, ClassNotFoundException, SQLException {
         jdbcUtils = new JdbcUtils();
     }
-
-
     @Override
-    public void addOrder(Order order) throws SQLException {
+    public List<Order> getAllOrders() throws SQLException {
+        List<Order> orderList = new ArrayList<>();
+        ProductRespository productRepository;
         try {
-            connection = jdbcUtils.getConnection();
-            String sql = "INSERT INTO Orders (orderId, tableId, employeeId, customerId, price) VALUES (" + order.getOrderId() + ", " + order.getTableId() + ", " + order.getEmployeeId() + ", " + order.getCustomerId() + ", '" + order.getTotalPaid() + "')";
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            jdbcUtils.closeConnection(connection);
-        }
-    }
+            connection = jdbcUtils.connect();
+            productRepository = new ProductRespository(); // tạo 1 lần
 
-    @Override
-    public void deleteOrder(int orderId, int tableId) throws SQLException {
-        try {
-            connection = jdbcUtils.getConnection();
-            String sql = "DELETE FROM Orders WHERE orderId = " + orderId + " AND tableId = " + tableId;
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            jdbcUtils.closeConnection(connection);
-        }
-    }
-
-    @Override
-    public int getOrderIdByTableId(int tableId) throws SQLException {
-        int orderId = 0;
-        try {
-            connection = jdbcUtils.getConnection();
-            String sql = "SELECT orderId FROM Orders WHERE tableId = " + tableId;
+            String sql = "SELECT * FROM OrderDetails";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
-            if (resultSet.next()) {
-                orderId = resultSet.getInt("orderId");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            jdbcUtils.closeConnection(connection);
-        }
-        return orderId; // Placeholder return value
-    }
 
-    @Override
-    public void updateOrder(Order order) throws SQLException {
-        try {
-            connection = jdbcUtils.getConnection();
-            String sql = "UPDATE Orders SET tableId = " + order.getTableId() + ", employeeId = " + order.getEmployeeId() + ", customerId = " + order.getCustomerId() + ", price = '" + order.getTotalPaid() + "' WHERE orderId = " + order.getOrderId();
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            jdbcUtils.closeConnection(connection);
-        }
-    }
-
-    @Override
-    public void getAllOrders() throws SQLException {
-        ArrayList<Order> orders = new ArrayList<>();
-        try {
-            connection = jdbcUtils.getConnection();
-            String sql = "SELECT * FROM Orders";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
-                int orderId = resultSet.getInt("orderId");
-                int tableId = resultSet.getInt("tableId");
-                int employeeId = resultSet.getInt("employeeId");
-                int customerId = resultSet.getInt("customerId");
-                double price = resultSet.getDouble("price");
-                Order order = new Order(orderId, tableId, employeeId, customerId, price);
-                orders.add(order);
+                int orderID = resultSet.getInt("orderID");
+                int employeeID = resultSet.getInt("employeeID");
+                int customerID = resultSet.getInt("customerID");
+                int tableID = resultSet.getInt("tableID");
+                String status = resultSet.getString("status");
+
+                // int productID = resultSet.getInt("productID");
+                // int quantity = resultSet.getInt("quantity");
+
+                // Map<Product, Integer> products = new HashMap<>();
+                // Product product = productRepository.getProductByID(productID);
+                // if (product != null) {
+                //     products.put(product, quantity);
+                // }
+                
+                Order order = new Order(orderID, employeeID, customerID, tableID, status);
+                orderList.add(order);
             }
-        } catch (SQLException e) {
+
+            statement.close();
+            resultSet.close();
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            jdbcUtils.closeConnection(connection);
+            if (connection != null) connection.close();
         }
+        return orderList;
+        
     }
-    
+
+   
+
 }
