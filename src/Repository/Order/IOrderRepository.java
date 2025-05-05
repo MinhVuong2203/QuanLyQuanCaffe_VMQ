e382176e672a8f908789116da2544036dd152220
sHunOrinn <161719997+sHunOrinn@users.noreply.github.com>
2025-05-05 15:21:47 +0700

    Discount

--- src/Repository/Order/IOrderRepository.java
+++ src/Repository/Order/IOrderRepository.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 public interface IOrderRepository {
     List<Order> getAllOrders() throws SQLException;
+
     public String getTimeByTableID(int tableID) throws SQLException;
+
     public List<Order> getOrdersBetweenDates(LocalDate fromDate, LocalDate toDate)
-        throws SQLException, IOException, ClassNotFoundException;
+            throws SQLException, IOException, ClassNotFoundException;
+
+    public void updateOrderDiscount(int orderId, double discountAmount)
+            throws SQLException, ClassNotFoundException, IOException;
 }
